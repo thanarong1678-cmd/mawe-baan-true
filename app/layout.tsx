cce6ff4dd6a9570674fcc8fceb0f017747b1cbae
thanarong1678-cmd <thanarong1678@gmail.com>
2026-09-14 13:31:59 +0700

    feat: enable improved house cat walking animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./cat-history-collapse.css";
+import "./cat-house-animation.css";
 import AuthGuard from "./auth-guard";
 import CatHistoryCollapse from "./cat-history-collapse";
 import CatHeaderDynamic from "./cat-header-dynamic";
